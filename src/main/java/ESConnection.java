6e0ae4a3d23add714d78f168f1b7b1b4414e31d2
kidinamoto01 <kidinamoto@qq.com>
2017-05-06 17:31:30 +0800

    Initial commit

---
+++ src/main/java/ESConnection.java
@@ -0,0 +1,67 @@
+/**
+ * Created by suyu on 17-5-6.
+ */
+
+
+import java.net.InetAddress;
+
+import java.net.UnknownHostException;
+
+
+import org.elasticsearch.action.search.SearchResponse;
+
+import org.elasticsearch.action.search.SearchType;
+
+import org.elasticsearch.client.transport.TransportClient;
+
+import org.elasticsearch.common.settings.Settings;
+
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+import org.elasticsearch.index.query.QueryBuilders;
+
+import org.elasticsearch.search.SearchHit;
+
+import org.elasticsearch.search.SearchHits;
+
+import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
+
+public class ESConnection {
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        Settings settings = Settings.builder().put("cluster.name", "elasticsearch").put("xpack.security.user", "sheshou:sheshou12345").put("client.transport.sniff", true).build();
+
+
+        TransportClient client = new PreBuiltXPackTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("42.123.99.38"), 9300));
+
+
+        //        GetResponse response = client.prepareGet("zq1", "first", "5d0b0c8bc042aca1a969b70d2e7dd942").execute().actionGet();
+
+        //        System.out.println(response.getSourceAsString());
+
+
+        SearchResponse searchResp = client.prepareSearch("zq1").setTypes("first").setSearchType(SearchType.QUERY_THEN_FETCH).setQuery(QueryBuilders.multiMatchQuery("华夏创新科技", "_all")) // Query
+
+                .setPostFilter(QueryBuilders.rangeQuery("pubdate").from("2016-02-12").to("2016-02-15")) // Filter
+
+                .setFrom(0).setSize(60).setExplain(true).get();
+
+
+        SearchHits hits = searchResp.getHits();
+
+        System.out.println(hits.getTotalHits());
+
+        for (SearchHit hit : hits.getHits()) {
+
+            System.out.println(hit.getSource());
+
+        }
+
+        client.close();
+
+    }
+
+
+}
+}
